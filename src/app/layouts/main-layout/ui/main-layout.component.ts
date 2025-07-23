f53a889e2159e7faa3da2823aec53dda78c8d5bf
maksimbilyk <pdo090318@gmail.com>
2025-07-23 19:07:02 +0300

    Add adptive side bar for mobile

--- src/app/layouts/main-layout/ui/main-layout.component.ts
+++ src/app/layouts/main-layout/ui/main-layout.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { CommonModule, NgIf } from '@angular/common';
+import { RouterModule, Router, NavigationEnd } from '@angular/router';
 import { NavigationSideBarComponent } from '../../../../widgets/navigation-side-bar';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [CommonModule, RouterModule, NavigationSideBarComponent],
+  imports: [CommonModule, RouterModule, NavigationSideBarComponent, NgIf],
   templateUrl: './main-layout.component.html',
 })
-export class MainLayoutComponent {}
+export class MainLayoutComponent implements OnInit, OnDestroy {
+  isSidebarOpen = false;
+  private routerSub?: Subscription;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.routerSub = this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.isSidebarOpen = false;
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.routerSub?.unsubscribe();
+  }
+
+  openSidebar() {
+    this.isSidebarOpen = true;
+  }
+
+  closeSidebar() {
+    this.isSidebarOpen = false;
+  }
+}
